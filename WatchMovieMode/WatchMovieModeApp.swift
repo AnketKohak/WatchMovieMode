663c9f14d12296c24ae846eeb745518c731c9c79
Anket Kohak <anketkohak40@gmail.com>
2025-01-22 13:06:01 +0530

    Initial Commit

---
+++ WatchMovieMode/WatchMovieModeApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchMovieModeApp.swift
+//  WatchMovieMode
+//
+//  Created by Anket Kohak on 22/01/25.
+//
+
+import SwiftUI
+
+@main
+struct WatchMovieModeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
